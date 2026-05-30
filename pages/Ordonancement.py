c0c91183edcca481c71847331bb10ffc6b25904f
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-05-30 18:28:24 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -7,7 +7,7 @@ from streamlit_autorefresh import st_autorefresh
 from datetime import datetime
 
 
-st_autorefresh(interval=15000, key="refresh_clock")
+st_autorefresh(interval=2000, key="refresh_clock")
 st.set_page_config(page_title="Ordonancement de la production", layout="wide")
 
 st.title("📦 Ordonancement de la production")
