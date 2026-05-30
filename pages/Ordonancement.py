d4b25d3ad67d26fd9e73729ee40a5f91554aa906
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-05-30 18:38:38 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -341,12 +341,12 @@ if "show_complete_gantt" not in st.session_state:
 col_gantt_btn1, col_gantt_btn2 = st.columns([1, 1], gap="large")
 
 with col_gantt_btn1:
-    if st.button("📊 Afficher Gantt Complete", type="primary", use_container_width=True):
+    if st.button("📊 Afficher Gantt Complete", type="primary"):
         st.session_state.show_complete_gantt = True
 
 with col_gantt_btn2:
     if st.session_state.show_complete_gantt:
-        if st.button("❌ Masquer le Diagramme", use_container_width=True):
+        if st.button("❌ Masquer le Diagramme" ):
             st.session_state.show_complete_gantt = False
             st.rerun()
 
