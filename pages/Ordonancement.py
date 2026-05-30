e57c27d85b61cea728b5a22b38bdf0daedd8d3e1
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-05-30 19:45:21 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -349,7 +349,7 @@ with col_gantt_btn1:
 
 with col_gantt_btn2:
     if st.session_state.show_complete_gantt:
-        if st.button("Masquer le Diagramme"):
+        if st.button("Masquer le Diagramme", type="Secondary"):
             st.session_state.show_complete_gantt = False
             st.rerun()
 
