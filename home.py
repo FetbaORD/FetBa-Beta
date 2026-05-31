7f83cb02fd5ae2e0db9f5bd2b0711f0fbbd7313a
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-05-31 08:30:21 +0100

    Update home.py

--- home.py
+++ home.py
@@ -159,7 +159,7 @@ if not st.session_state.is_activated:
             
             sub_b1, sub_b2, sub_b3 = st.columns([2, 1.5, 2])
             with sub_b2:
-                if st.button("Acheter une Clé / Support", type="secondary",use_container_width=True):
+                if st.button("Acheter une Clé / Support", type="secondary",use_container_width=False):
                     st.toast("💡 Redirection vers le support commercial...", icon="ℹ️")
                     show_contact_dialog() 
                     
