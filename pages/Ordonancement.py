f65a6b920936d5f8c3018565e09e4785a2525454
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-05-31 09:55:48 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -131,7 +131,7 @@ if "sequence_df" in st.session_state:
     
     with col_btn:
         # استخدام popover لإنشاء قائمة منسدلة تحتوي على الخوارزميات
-        with st.popover("🔀 Choisir Algorithme"):
+        with st.popover("🔀 Choisir Algorithme" , type="primary"):
             st.write("Sélectionnez un Algorithme :")
             
             # إنشاء الأزرار الخمسة
