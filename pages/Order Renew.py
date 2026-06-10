eb5f68ee394333e39a3d6d8d393f3a6dfa870965
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-06-10 19:07:52 +0100

    Update Order Renew.py

--- pages/Order Renew.py
+++ pages/Order Renew.py
@@ -124,7 +124,7 @@ if submit_button:
             "Prix": price,
             "Méthode de paiement": payment_method,
             "_captcha": "false",
-            "_subject": f"Nouvelle demande de renouvellement : {serial_number}"
+            "_subject": f"Nouvelle demande de renouvellement : {serial_number}", # تأكد من وجود هذه الفاصلة هنا
             "_replyto": "customer@example.com"  # هامة جداً لتفادي فلاتر الحظر البريدي للمرفقات
         }
 
