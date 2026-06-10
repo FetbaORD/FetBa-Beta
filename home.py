8a6f0b2e1e813c91befc2bf8d468670d2f145379
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-06-10 09:08:43 +0100

    Update home.py

--- home.py
+++ home.py
@@ -250,7 +250,7 @@ if not st.session_state.is_activated:
     # زر إضافي أسفل البطاقات لتوجيههم مباشرة للتواصل
     c1, c2, c3 = st.columns([1, 1.5, 1])
     with c2:
-        if st.button("Demander un abonnement maintenant", icon=":material/shopping_bag:", key="pricing_btn", use_container_width=True):
+        if st.button("Demander un abonnement maintenant", icon=":material/shopping_bag:", key="pricing_btn", use_container_width=False):
             show_contact_dialog()
 
     st.stop() # إيقاف الكود هنا لحماية بقية التطبيق الداخلي
