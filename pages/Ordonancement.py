1505a3c63c316421dbee580d4d9ca1786c858c81
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-09-25 19:45:40 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -490,14 +490,15 @@ if "Pij" in st.session_state and "sequence" in st.session_state:
     start_times = np.zeros((n_j, n_m))
     end_times = np.zeros((n_j, n_m))
 
-	if "sim_start_time" in st.session_state:
+    # الحصول على الوقت الحالي للمحاكاة (ثواني)
+    if "sim_start_time" in st.session_state:
 		real_elapsed_time = (
 			datetime.now() - st.session_state.sim_start_time
 		).total_seconds()
-		# تطبيق سرعة المحاكاة على الـ Gantt
-        current_sim_time = real_elapsed_time * simulation_speed
-    else:
-        current_sim_time = 0
+		# تطبيق سرعة المحاكاة
+		current_sim_time = real_elapsed_time * simulation_speed
+	else:
+		current_sim_time = 0
 		
     # حساب الجدولة الديناميكية (Flow Shop مع إزاحة الأعطال)
     for j_idx, job_id in enumerate(sequence):
