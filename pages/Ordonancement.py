2824d199adb228ac350d268ed74871616f89cc1d
Merve_Dz <mohamedbachirhabchi@gmail.com>
2026-09-25 19:43:00 +0100

    Update Ordonancement.py

--- pages/Ordonancement.py
+++ pages/Ordonancement.py
@@ -490,13 +490,11 @@ if "Pij" in st.session_state and "sequence" in st.session_state:
     start_times = np.zeros((n_j, n_m))
     end_times = np.zeros((n_j, n_m))
 
-	# الحصول على الوقت الحالي للمحاكاة (ثواني)
-    if "sim_start_time" in st.session_state:
-	    real_elapsed_time = (
-            datetime.now() - st.session_state.sim_start_time
-        ).total_seconds()
-
-        # تطبيق سرعة المحاكاة على الـ Gantt
+	if "sim_start_time" in st.session_state:
+		real_elapsed_time = (
+			datetime.now() - st.session_state.sim_start_time
+		).total_seconds()
+		# تطبيق سرعة المحاكاة على الـ Gantt
         current_sim_time = real_elapsed_time * simulation_speed
     else:
         current_sim_time = 0
